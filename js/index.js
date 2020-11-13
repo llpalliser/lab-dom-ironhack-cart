cb1c0783f0e2bc96b1314aca6cd846a0eff30464
Llorenç Palliser <palliser@palliser.es>
2020-11-13 19:02:15 +0100

    Bug: no me borra los elementos creados

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
   let price = Number(product.querySelector('.price span').innerText)
   let quantity = Number(product.querySelector('.quantity input').valueAsNumber)
@@ -9,24 +8,28 @@ function updateSubtotal(product) {
   let subtotalSpan = product.querySelector('.subtotal span')
 
   subtotalSpan.innerText = subtotalPrice;
-}
 
+  return subtotalPrice;
+
+}
 
 // ITERATION 2 & 3
 
 function calculateAll() {
 
-
   let total = 0;
   const products = document.querySelectorAll('.product');
   products.forEach(product => {
+
     updateSubtotal(product)
-    total += Number(product.querySelector('.subtotal span').innerText)
+
+    total += updateSubtotal(product)
     document.querySelector('#total-value span').innerText = total
-    console.log(total)
+
   });
 }
 
+// ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
@@ -37,23 +40,33 @@ function removeProduct(event) {
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+
+  const tableProducts = document.querySelector("tbody")
+
+  let productInput = document.getElementById('inputProduct').value
+  let productPrice = document.getElementById('inputPrice').value
+
+  let template = `<td class="name"> <span>${productInput}</span>        </td>        <td class="price">$<span>${productPrice}</span></td>        <td class="quantity">    <input type="number" value="0" min="0" placeholder="Quantity" />        </td>        <td class="subtotal">$<span>0</span></td>        <td class="action">          <button class="btn btn-remove">Remove</button>        </td>`
+  let row = tableProducts.insertRow();
+  row.className = 'product';
+  row.innerHTML = template;
+
+  calculateAll()
+
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  let boto = document.querySelector('btn-remove');
-
   const tableProducts = document.querySelectorAll('.product');
-
   tableProducts.forEach(product => {
-    product.querySelector('.btn-remove').addEventListener('click', removeProduct)
-
-    console.log("botó actiu")
+    product.querySelector('.btn').addEventListener('click', removeProduct)
   });
 
-  //... your code goes here
+  const newProductButton = document.getElementById('create');
+  newProductButton.addEventListener('click', createProduct);
 }
+    
 )
+
